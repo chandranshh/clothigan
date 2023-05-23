09c5e47fe4285afa625f1c5bf64f63950373be40
chandranshh <chandranshyay@gmail.com>
2023-05-23 20:56:25 +0530

    added user model schema for userModel for MongoDB

---
+++ server/models/Users.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    profilePicture: {
+      type: String,
+      default: "",
+    },
+    phone: {
+      type: String,
+      default: "",
+      match: /^\d{10}$/, // Matches a 10-digit phone number format
+    },
+    address: {
+      type: String,
+      default: "",
+    },
+    address2: {
+      type: String,
+      default: "",
+    },
+    token: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
+
+const Users = mongoose.model("User", userSchema); // Use singular "User" for the model name
+
+module.exports = Users;
