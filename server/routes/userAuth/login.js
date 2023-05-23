2ff69aa20d082cd94fc1b37659c31f152203d42c
chandranshh <chandranshyay@gmail.com>
2023-05-23 21:28:43 +0530

    made few chnages in the userAuth api

--- server/routes/userAuth/login.js
+++ server/routes/userAuth/login.js
@@ -3,6 +3,11 @@ const router = express.Router();
 const bcrypt = require("bcryptjs");
 const Users = require("../../models/Users");
 const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const jwt_secret = process.env.JWT_SECRET;
 
 router.post("/", async (req, res) => {
   const { email, password } = req.body;
@@ -16,7 +21,32 @@ router.post("/", async (req, res) => {
       if (!isMatch) {
         return res.status(400).json({ error: "Invalid credentials" });
       } else {
-        res.status(200).json(findUser);
+        jwt.sign(
+          {
+            id: findUser._id,
+            email: findUser.email,
+          },
+          jwt_secret,
+          { expiresIn: 86000 },
+          (err, token) => {
+            if (err) {
+              console.error(err);
+              return res.status(500).json({ error: "Server error" });
+            }
+            findUser.token = token;
+            findUser.save();
+            return res
+              .cookie("token", token)
+              .status(200)
+              .json({
+                token,
+                user: {
+                  id: findUser._id,
+                  email: findUser.email,
+                },
+              });
+          }
+        );
       }
     }
   } catch (error) {
