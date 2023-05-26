85aad0e89a9eb8a6fa30a1860b01418c7d784d12
chandranshh <chandranshyay@gmail.com>
2023-05-27 01:39:19 +0530

    added add and remove feature in cart component

--- client/src/features/slices/cartSlice.js
+++ client/src/features/slices/cartSlice.js
@@ -20,9 +20,32 @@ const cartSlice = createSlice({
         state.cartQuantity++;
       }
     },
+    increaseQuantity: (state, action) => {
+      const existingItem = state.cartItems.find(
+        (item) => item.id === action.payload.id
+      );
+      if (existingItem) {
+        existingItem.quantity++;
+      }
+    },
+    decreaseQuantity: (state, action) => {
+      const existingItem = state.cartItems.find(
+        (item) => item.id === action.payload.id
+      );
+      if (existingItem) {
+        existingItem.quantity--;
+      }
+    },
+    removeFromCart: (state, action) => {
+      state.cartItems = state.cartItems.filter(
+        (item) => item.id !== action.payload.id
+      );
+      state.cartQuantity--;
+    },
   },
 });
 
-export const { addToCart } = cartSlice.actions;
+export const { addToCart, increaseQuantity, decreaseQuantity, removeFromCart } =
+  cartSlice.actions;
 
 export default cartSlice.reducer;
--- client/src/pages/cart/CartProduct.jsx
+++ client/src/pages/cart/CartProduct.jsx
@@ -1,17 +1,46 @@
 /* eslint-disable react/prop-types */
+import { Button } from "@chakra-ui/react";
+import { useDispatch } from "react-redux";
+import {
+  increaseQuantity,
+  decreaseQuantity,
+  removeFromCart,
+} from "../../features/slices/cartSlice";
 
-// eslint-disable-next-line react/prop-types
 function CartProduct({ item }) {
-  console.log(item);
+  const dispatch = useDispatch();
+
+  const handleIncreaseQuantity = () => {
+    dispatch(increaseQuantity({ id: item.id }));
+  };
+
+  const handleDecreaseQuantity = () => {
+    if (item.quantity > 1) {
+      dispatch(decreaseQuantity({ id: item.id }));
+    } else {
+      dispatch(removeFromCart({ id: item.id }));
+    }
+  };
+
   return (
     <div className="flex my-3 rounded-md m-8 h-[130px] shadow-xl">
       <div className="h-[100px] w-[100px]">
         <img className="p-4" src={item.image} alt={item.id} />
       </div>
-      <div>
+      <div className="mt-10">
         <div>{item.title}</div>
-        <div>{item.price}</div>
-        <div>{item.quantity}</div>
+        <div>Rs. {item.price}</div>
+      </div>
+      <div className="flex flex-col">
+        <div className="flex gap-5 inline-block mt-[5rem]">
+          <Button size="sm" onClick={handleDecreaseQuantity}>
+            -
+          </Button>
+          <div>{item.quantity}</div>
+          <Button size="sm" onClick={handleIncreaseQuantity}>
+            +
+          </Button>
+        </div>
       </div>
     </div>
   );
